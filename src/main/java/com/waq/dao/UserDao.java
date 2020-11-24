fd28466dbf4c0e1a1f6fcf21aee3227380edca9a
Kraty <1457659929@qq.com>
2020-11-24 21:09:44 +0800

    基于注解的mybatis使用

--- src/main/java/com/waq/dao/UserDao.java
+++ src/main/java/com/waq/dao/UserDao.java
@@ -1,12 +1,14 @@
 package com.waq.dao;
 
 import com.waq.domain.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface UserDao {
 
     // 查询所有操作
+    @Select("select * from user")
     List<User> findAll();
 
 }
