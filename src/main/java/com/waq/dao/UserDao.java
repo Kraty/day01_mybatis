32561360403774e0dc7c62a388b4ee00e044e8a7
Kraty <1457659929@qq.com>
2020-11-25 18:45:00 +0800

    简单的CRUD操作

--- src/main/java/com/waq/dao/UserDao.java
+++ src/main/java/com/waq/dao/UserDao.java
@@ -11,4 +11,18 @@ public interface UserDao {
     @Select("select * from user")
     List<User> findAll();
 
+    // 增加用户
+    @Select("insert into user (username, address, sex, birthday) values " +
+            "(#{username}, #{address}, #{sex}, #{birthday})")
+    void saveUser(User user);
+
+    // 删除用户
+    @Select("delete from user where id=#{id}")
+    void deleteUser(Integer id);
+
+    // 修改用户
+    @Select("update user set username=#{username}, address=#{address}, " +
+            "sex=#{sex}, birthday=#{birthday} where id=#{id}")
+    void updateUser(User user);
+
 }
--- src/test/java/com/waq/test/MybatisTest.java
+++ src/test/java/com/waq/test/MybatisTest.java
@@ -6,9 +6,13 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -18,24 +22,79 @@ import java.util.List;
 
 public class MybatisTest {
 
-    // 入门案例
-    public static void main(String[] args) throws IOException {
+    private InputStream in;
+    private SqlSession sqlSession;
+    private UserDao userDao;
 
-        // 1.读取配置文件
-        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
-        // 2.创建SQL session factory工厂
-        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory factory = builder.build(in);
-        // 3.使用工厂生产一个SQL session对象
-        SqlSession sqlSession = factory.openSession();
-        // 4.使用SQL session创建dao接口的代理对象
-        UserDao userDao = sqlSession.getMapper(UserDao.class);
+    // 查询所有用户
+    @Test
+    public void testFindAll() {
+
         // 5.使用代理对象执行方法
         List<User> users = userDao.findAll();
         for (User user : users) {
             System.out.println(user);
         }
-        // 6.释放资源
+
+
+    }
+
+    // 添加新用户
+    @Test
+    public void testSaveUser() {
+
+        User user = new User();
+        user.setUsername("小花");
+        user.setAddress("北京顺兴区");
+        user.setSex("男");
+        user.setBirthday(new Date());
+        userDao.saveUser(user);
+
+    }
+
+    // 删除用户
+    @Test
+    public void testDeleteUser() {
+
+        userDao.deleteUser(49);
+
+    }
+
+    // 修改用户
+    @Test
+    public void testUpdateUser() {
+
+        User user = new User();
+        user.setId(50);
+        user.setUsername("小花");
+        user.setAddress("北京顺兴区");
+        user.setSex("女");
+        user.setBirthday(new Date());
+        userDao.updateUser(user);
+
+    }
+
+    @Before
+    public void init() throws Exception {
+
+        // 1.读取配置文件
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        // 2.创建SQL session factory工厂
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+        // 3.使用工厂生产一个SQL session对象
+        sqlSession = factory.openSession();
+        // 4.使用SQL session创建dao接口的代理对象
+        userDao = sqlSession.getMapper(UserDao.class);
+
+    }
+
+    @After
+    public void destroy() throws IOException {
+
+        // 事务提交
+        sqlSession.commit();
+
+        // 释放资源
         sqlSession.close();
         in.close();
 
